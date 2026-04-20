019972433adb0be1aab077bedb75d621714b0be9
ethachu21 <ethachu21@gmail.com>
2026-04-20 06:27:54 +0000

    Hail mary
    im going insane, i have not slept in a while

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ Entry point script!
 """
 
 from Core.bot import bot
+from Data import initialize_database
 import dotenv
 import os
 import sys
+
 sys.dont_write_bytecode = True
 dotenv.load_dotenv()
+
+# load the db before the bot or else... heartbeat issues
+initialize_database()
+
 token = os.getenv("TOKEN")
 assert type(token) == str
 bot.run(token)
