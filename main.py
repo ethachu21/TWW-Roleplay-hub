c90eefa21f3282c1ecd86c9f067e10e3f70a4ed6
ethachu21 <ethachu21@gmail.com>
2026-04-20 05:14:12 +0000

    Do not write bytecode!

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Entry point script!
 from Core.bot import bot
 import dotenv
 import os
+import sys
+sys.dont_write_bytecode = True
 dotenv.load_dotenv()
 token = os.getenv("TOKEN")
 assert type(token) == str
